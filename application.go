9671ab2b2e3eafccec912f0aaafcba1d97474cdc
IchabodDee <michael.dee33@gmail.com>
2014-05-31 17:48:56 -0400

    Started using the standard go wrapper technique.  Structs to catch the tutum API JSON responses.

--- application.go
+++ application.go
@@ -5,49 +5,51 @@ import (
 	"log"
 )
 
-type CListResponse struct {
-	Objects []Container `json:"objects"`
+type AListResponse struct {
+	Objects []Application `json:"objects"`
 }
 
-type Container struct {
-	Application            string    `json:"application"`
+type Application struct {
 	Autodestroy            string    `json:"autodestroy"`
 	Autoreplace            string    `json:"autoreplace"`
 	Autorestart            string    `json:"autorestart"`
-	Container_ports        []CCPInfo `json:"container_ports"`
+	Container_ports        []ACPInfo `json:"container_ports"`
 	Container_size         string    `json:"container_size"`
 	Current_num_containers int       `json:"current_num_containers"`
 	Deployed_datetime      string    `json:"deployed_datetime"`
 	Destroyed_datetime     string    `json:"destroyed_datetime"`
 	Entrypoint             string    `json:"entrypoint"`
-	Exit_code              int       `json:"exit_code"`
-	Exit_code_message      string    `json:"exit_code_message"`
 	Image_name             string    `json:"image_name"`
 	Image_tag              string    `json:"image_tag"`
 	Name                   string    `json:"name"`
+	Sequential_deployment  bool      `json:"sequential_deployment"`
 	Public_dns             string    `json:"public_dns"`
 	Resource_uri           string    `json:"resource_uri"`
 	Run_command            string    `json:"run_command"`
+	Running_num_containers int       `json:"running_num_containers"`
 	Started_datetime       string    `json:"started_datetime"`
 	State                  string    `json:"state"`
 	Stopped_datetime       string    `json:"stopped_datetime"`
+	Stopped_num_containers int       `json:"stopped_num_containers"`
+	Target_num_containers  int       `json:"target_num_containers"`
 	Unique_name            string    `json:"unique_name"`
 	Uuid                   string    `json:"uuid"`
+	Web_public_dns         string    `json:"web_public_dns"`
 }
 
-type CCPInfo struct {
-	Container  string `json:"container"`
-	Inner_port int    `json:"inner_port"`
-	Outer_port int    `json:"outer_port"`
-	Protocol   string `json:"protocol"`
+type ACPInfo struct {
+	Application string `json:"application"`
+	Inner_port  int    `json:"inner_port"`
+	Outer_port  int    `json:"outer_port"`
+	Protocol    string `json:"protocol"`
 }
 
-func ListContainers() []Container {
+func ListApplications() []Application {
 
-	url := "container/"
+	url := "application/"
 	request := "GET"
 
-	var response CListResponse
+	var response AListResponse
 	data := TutumCall(url, request)
 	err := json.Unmarshal(data, &response)
 	if err != nil {
--- gotutum.go
+++ gotutum.go
@@ -1,54 +1,53 @@
 package tutum
 
 import (
-	"container"
-	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 var User string
 var Apikey string
 
 func AuthForUser() string {
-
+	//Still need to check init file.  Look at python to see how to do it.
+	//First init file, then ENV VAR, then none.
+	//Check if the User and Apikey are set.  If not grab from the environment variables.
+	if User == "" {
+		if os.Getenv("TUTUM_USER") != "" {
+			User = os.Getenv("TUTUM_USER")
+		}
+	}
+	if Apikey == "" {
+		if os.Getenv("TUTUM_APIKEY") != "" {
+			Apikey = os.Getenv("TUTUM_APIKEY")
+		}
+	}
 	return "ApiKey " + User + ":" + Apikey
 }
 
-// func ListContainers() interface{} {
+func BaseUrl() string {
+	var baseUrl string
+	if os.Getenv("TUTUM_BASE_URL") != "" {
+		url = os.Getenv("TUTUM_BASE_URL")
+	} else {
+		baseUrl = "https://app.tutum.co/api/v1/"
+	}
+	return baseUrl
+}
 
-// 	url := "https://app.tutum.co/api/v1/container/"
-// 	request := "GET"
-
-// 	f := TutumCall(url, request)
-// 	m := f.(map[string]interface{})
-
-// 	//Relies on the array of items being named objects.
-// 	for k, v := range m {
-// 		if k == "objects" {
-// 			return v
-// 		}
-// 	}
-// 	return nil
-// }
-
-func TutumCall(url string, requestType string) interface{} {
+func TutumCall(url string, requestType string) []byte {
 
 	client := &http.Client{}
-	req, err := http.NewRequest(requestType, url, nil)
-
+	req, err := http.NewRequest(requestType, BaseUrl()+url, nil)
 	req.Header.Add("Authorization", AuthForUser())
 	req.Header.Add("Accept", "application/json")
-
 	response, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
 	}
 	data, err := ioutil.ReadAll(response.Body)
 
-	var f interface{}
-	err = json.Unmarshal(data, &f)
-
-	return f
+	return data
 }
