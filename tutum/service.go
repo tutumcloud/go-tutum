e01b2c577f9213ce0f4719d20ba599f427fdab3d
Maxime Heckel <heckelmaxime@gmail.com>
2015-04-22 18:48:50 +0200

    add log support for services

--- tutum/service.go
+++ tutum/service.go
@@ -7,31 +7,33 @@ type SListResponse struct {
 }
 
 type Service struct {
-	Autodestroy            string    `json:"autodestroy"`
-	Autoreplace            string    `json:"autoreplace"`
-	Autoredeploy           bool      `json:"autoredeploy"`
-	Autorestart            string    `json:"autorestart"`
-	Containers             []string  `json:"containers"`
-	Container_ports        []SCPInfo `json:"container_ports"`
-	Container_size         string    `json:"container_size"`
-	Current_num_containers int       `json:"current_num_containers"`
-	Deployed_datetime      string    `json:"deployed_datetime"`
-	Destroyed_datetime     string    `json:"destroyed_datetime"`
-	Entrypoint             string    `json:"entrypoint"`
-	Exit_code              int       `json:"exit_code"`
-	Exit_code_message      string    `json:"exit_code_message"`
-	Image_name             string    `json:"image_name"`
-	Image_tag              string    `json:"image_tag"`
-	Name                   string    `json:"name"`
-	Public_dns             string    `json:"public_dns"`
-	Resource_uri           string    `json:"resource_uri"`
-	Run_command            string    `json:"run_command"`
-	Started_datetime       string    `json:"started_datetime"`
-	State                  string    `json:"state"`
-	Stack                  string    `json:"stack"`
-	Stopped_datetime       string    `json:"stopped_datetime"`
-	Unique_name            string    `json:"unique_name"`
-	Uuid                   string    `json:"uuid"`
+	Autodestroy            string       `json:"autodestroy"`
+	Autoreplace            string       `json:"autoreplace"`
+	Autoredeploy           bool         `json:"autoredeploy"`
+	Autorestart            string       `json:"autorestart"`
+	Containers             []string     `json:"containers"`
+	Container_ports        []SCPInfo    `json:"container_ports"`
+	Container_size         string       `json:"container_size"`
+	Current_num_containers int          `json:"current_num_containers"`
+	Deployed_datetime      string       `json:"deployed_datetime"`
+	Destroyed_datetime     string       `json:"destroyed_datetime"`
+	Entrypoint             string       `json:"entrypoint"`
+	Exit_code              int          `json:"exit_code"`
+	Exit_code_message      string       `json:"exit_code_message"`
+	Image_name             string       `json:"image_name"`
+	Image_tag              string       `json:"image_tag"`
+	Linked_to_service      []LinkToInfo `json:"linked_to_service"`
+	Name                   string       `json:"name"`
+	Public_dns             string       `json:"public_dns"`
+	Resource_uri           string       `json:"resource_uri"`
+	Run_command            string       `json:"run_command"`
+	Started_datetime       string       `json:"started_datetime"`
+	State                  string       `json:"state"`
+	Stack                  string       `json:"stack"`
+	Stopped_datetime       string       `json:"stopped_datetime"`
+	Target_num_containers  int          `json:"target_num_containers"`
+	Unique_name            string       `json:"unique_name"`
+	Uuid                   string       `json:"uuid"`
 }
 
 type SCPInfo struct {
@@ -39,6 +41,18 @@ type SCPInfo struct {
 	Inner_port int    `json:"inner_port"`
 	Outer_port int    `json:"outer_port"`
 	Protocol   string `json:"protocol"`
+}
+
+type LinkToInfo struct {
+	From_service string `json:"from_service"`
+	Name         string `json:"name"`
+	To_service   string `json:"to_service"`
+}
+
+type BasicService struct {
+	Image                 string `json:"image"`
+	Name                  string `json:"name"`
+	Target_num_containers int    `json:"target_num_containers"`
 }
 
 func ListServices() ([]Service, error) {
@@ -76,5 +90,32 @@ func GetService(uuid string) (Service, error) {
 	}
 
 	return response, nil
+
+}
+
+func CreateService(newService BasicService) (Service, error) {
+
+	/*url := "service/"
+	request := "POST"*/
+	//TUTUM CALL WITH BYTE BODY REQUEST
+	var response Service
+
+	return response, nil
+
+}
+
+func GetServiceLogs(uuid string) (string, error) {
+
+	url := "service/" + uuid + "/logs/"
+	request := "GET"
+
+	data, err := TutumCall(url, request)
+	if err != nil {
+		return "", err
+	}
+
+	s := string(data)
+
+	return s, nil
 
 }
