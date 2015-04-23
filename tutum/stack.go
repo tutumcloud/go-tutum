32f86f57c0b86b23bc5baeb2d7ed7b94e6461269
Maxime Heckel <heckelmaxime@gmail.com>
2015-04-23 18:02:45 +0200

    Add Stack struct

--- tutum/stack.go
+++ tutum/stack.go
@@ -1,1 +1,39 @@
 package tutum
+
+import "encoding/json"
+
+type StackListResponse struct {
+	Objects []Stack `json: "objects"`
+}
+
+type Stack struct {
+	Deployed_datetime  string   `json:"deployed_datetime"`
+	Destroyed_datetime string   `json:"destroyed_datetime"`
+	Name               string   `json:"name"`
+	Resource_uri       string   `json:"resource_uri"`
+	Service            []string `json:"services"`
+	State              string   `json:"state"`
+	Synchronized       bool     `json:"synchronized"`
+	Uuid               string   `json:"uuid"`
+}
+
+func ListStacks() ([]Stack, error) {
+	url := "stack/"
+	request := "GET"
+
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response StackListResponse
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Objects, nil
+}
