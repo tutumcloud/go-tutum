b1b8e7f04adbc86ca77ce08d8a15cf80797ca720
Maxime Heckel <heckelmaxime@gmail.com>
2015-04-24 18:48:21 +0200

    Add Token endpoint

---
+++ tutum/token.go
@@ -0,0 +1,26 @@
+package tutum
+
+import "encoding/json"
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+func CreateToken() (Token, error) {
+	url := "token/"
+	request := "POST"
+	body := []byte(`{}`)
+	var response Token
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+}
