450d127fd76d5e7c44987bfbffe37344382f3cc2
Maxime Heckel <heckelmaxime@gmail.com>
2015-04-24 13:48:53 +0200

    Add Action endpoints

--- tutum/action.go
+++ tutum/action.go
@@ -1,1 +1,63 @@
 package tutum
+
+import "encoding/json"
+
+type ActionListResponse struct {
+	Objects []Action `json:"objects"`
+}
+
+type Action struct {
+	Action       string `json:"action"`
+	Body         string `json:"body"`
+	End_date     string `json:"end_date"`
+	Ip           string `json:"ip"`
+	Location     string `json:"location"`
+	Logs         string `json:"logs"`
+	Method       string `json:"method"`
+	Object       string `json:"object"`
+	Path         string `json:"path"`
+	Resource_uri string `json:"resource_uri"`
+	Start_date   string `json:"start_date"`
+	State        string `json:"state"`
+	Uuid         string `json:"uuid"`
+}
+
+func ListActions() ([]Action, error) {
+	url := "action/"
+	request := "GET"
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response ActionListResponse
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Objects, nil
+}
+
+func GetAction(uuid string) (Action, error) {
+	url := "action/" + uuid + "/"
+	request := "GET"
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response Action
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+}
