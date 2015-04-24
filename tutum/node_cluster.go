2f3d0c2792ebb622632c57cc95a82f39dbb6a400
Maxime Heckel <heckelmaxime@gmail.com>
2015-04-24 11:50:47 +0200

    Add NodeCluster endpoints

---
+++ tutum/node_cluster.go
@@ -0,0 +1,168 @@
+package tutum
+
+import "encoding/json"
+
+type NodeClusterListResponse struct {
+	Objects []NodeCluster `json:"objects"`
+}
+
+type NodeCluster struct {
+	Current_num_nodes  int       `json:"current_num_nodes"`
+	Deployed_datetime  string    `json:"deployed_datetime"`
+	Destroyed_datetime string    `json:"destroyed_datetime"`
+	Disk               int       `json:"disk"`
+	Nodes              []string  `json:"nodes"`
+	Region             string    `json:"region"`
+	Resource_uri       string    `json:"resource_uri"`
+	State              string    `json:"state"`
+	Tags               []NodeTag `json:"tags"`
+	Target_num_nodes   int       `json:"target_num_nodes"`
+	Uuid               string    `json:"uuid"`
+}
+
+func ListNodeClusters() ([]NodeCluster, error) {
+
+	url := "nodecluster/"
+	request := "GET"
+
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response NodeClusterListResponse
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Objects, nil
+}
+
+func GetNodeCluster(uuid string) (NodeCluster, error) {
+
+	url := "nodecluster/" + uuid + "/"
+	request := "GET"
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response NodeCluster
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+}
+
+func CreateNodeCluster(newCluster []byte) (NodeCluster, error) {
+
+	url := "nodecluster/"
+	request := "POST"
+
+	var response NodeCluster
+
+	data, err := TutumCall(url, request, newCluster)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+}
+
+func DeployNodeCluster(uuid string) (NodeCluster, error) {
+
+	url := "nodecluster/" + uuid + "/deploy/"
+	request := "POST"
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response NodeCluster
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+}
+
+func UpdateNodeCluster(uuid string, updatedNode []byte) (NodeCluster, error) {
+
+	url := "nodecluster/" + uuid + "/"
+	request := "PATCH"
+
+	var response NodeCluster
+
+	data, err := TutumCall(url, request, updatedNode)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+}
+
+func UpgradeClusterDaemon(uuid string) (NodeCluster, error) {
+
+	url := "nodecluster/" + uuid + "/docker-upgrade/"
+	request := "POST"
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response NodeCluster
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+}
+
+func TerminateNodeCluster(uuid string) (NodeCluster, error) {
+
+	url := "node/"
+	request := "DELETE"
+	//Empty Body Request
+	body := []byte(`{}`)
+	var response NodeCluster
+
+	data, err := TutumCall(url, request, body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &response)
+	if err != nil {
+		panic(err)
+	}
+
+	return response, nil
+
+}
